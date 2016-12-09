5be0cb265f3167960b01dcba3fef6c61537739c6
Jake 'Sid' Smith <jake@dabapps.com>
2016-12-09 14:35:55 +0000

    3 space indentation

--- bin/cmd.js
+++ bin/cmd.js
@@ -10,16 +10,16 @@ var argv = require('yargs')
       default : '_dereq_',
       describe: 'token to change the variable into'
   })
-   .options('f', {
+  .options('f', {
       alias : 'from',
       default : 'require',
       describe: 'token to find and change'
   })
-   .help('h')
-   .alias('h', 'help')
-   .version(require('../package.json').version, 'v')
-   .alias('v', 'version')
-   .argv;
+  .help('h')
+  .alias('h', 'help')
+  .version(require('../package.json').version, 'v')
+  .alias('v', 'version')
+  .argv;
 
 var file = argv._[0];
 var input;
